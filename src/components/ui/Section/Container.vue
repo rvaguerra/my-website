2608893988b7bdbb974ef547289d26b4cda3b3ba
Rodrigo Viégas <rvaguerra@gmail.com>
2021-01-14 15:29:57 -0300

    Added section component

---
+++ src/components/ui/Section/Container.vue
@@ -0,0 +1,25 @@
+<template>
+  <article class="container mx-auto py-16">
+    <SectionTitle class="pb-8" :title="title"></SectionTitle>
+
+    <div class="tracking-wider leading-loose">
+      <slot></slot>
+    </div>
+  </article>
+</template>
+
+<script>
+import SectionTitle from "./Title";
+
+export default {
+  components: {
+    SectionTitle,
+  },
+  props: {
+    title: {
+      type: String,
+      default: "Section Title",
+    },
+  },
+};
+</script>
---
+++ src/components/ui/Section/Title.vue
@@ -0,0 +1,14 @@
+<template>
+  <h2 class="capitalize font-medium text-3xl">{{ title }}</h2>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: "Section Title",
+    },
+  },
+};
+</script>
