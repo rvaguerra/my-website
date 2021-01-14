89325a861cd8939ad05cf9d7c552402ec86b6682
Rodrigo Viégas <rvaguerra@gmail.com>
2021-01-14 15:12:31 -0300

    Added basic functionality

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,10 +1,16 @@
 <template>
-  <nav>
-    <ul>
-      <li>Hello</li>
-      <li>Skills</li>
-      <li>My Work</li>
-      <li>Testimonials</li>
+  <nav class="my-8">
+    <ul class="flex justify-center space-x-16">
+      <li class="navbar-item"><a href="#hello">Hello</a></li>
+      <li class="navbar-item"><a href="#skills">Skills</a></li>
+      <li class="navbar-item"><a href="#my-work">My Work</a></li>
+      <li class="navbar-item"><a href="#testimonials">Testimonials</a></li>
     </ul>
   </nav>
 </template>
+
+<style lang="postcss" scoped>
+.navbar-item > a {
+  @apply text-sm uppercase;
+}
+</style>
