9d6f9d0eb6baab0eb0e6bef437b3c278219c1cb5
Rodrigo Viégas <rvaguerra@gmail.com>
2021-01-15 16:57:35 -0300

    Update Container.vue
    HTML structure refactoring

--- src/components/ui/Section/Container.vue
+++ src/components/ui/Section/Container.vue
@@ -1,17 +1,19 @@
 <template>
-  <article class="container mx-auto py-32">
-    <SectionTitle v-if="$slots.title">
-      <slot name="title"></slot>
-    </SectionTitle>
+  <section>
+    <div class="container mx-auto py-32">
+      <SectionTitle v-if="$slots.title">
+        <slot name="title"></slot>
+      </SectionTitle>
 
-    <SectionSubTitle v-if="$slots['sub-title']">
-      <slot name="sub-title"></slot>
-    </SectionSubTitle>
+      <SectionSubTitle v-if="$slots['sub-title']">
+        <slot name="sub-title"></slot>
+      </SectionSubTitle>
 
-    <div class="mt-8 tracking-wider leading-loose">
-      <slot></slot>
+      <div class="mt-8 tracking-wider leading-loose">
+        <slot></slot>
+      </div>
     </div>
-  </article>
+  </section>
 </template>
 
 <script>
