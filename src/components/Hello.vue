e4650b68700277fe56717e1257b09acf38f20bb7
Rodrigo Viégas <rvaguerra@gmail.com>
2021-01-15 16:58:54 -0300

    HTML structure refactoring

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,12 +1,9 @@
 <template>
-  <section>
-    <SectionContainer title="Hello">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, dolorum
-      sunt sint quibusdam maiores non pariatur assumenda quo molestiae velit
-      minima eveniet praesentium et reiciendis suscipit. Ut omnis commodi
-      laborum.
-    </SectionContainer>
-  </section>
+  <SectionContainer>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, dolorum
+    sunt sint quibusdam maiores non pariatur assumenda quo molestiae velit
+    minima eveniet praesentium et reiciendis suscipit. Ut omnis commodi laborum.
+  </SectionContainer>
 </template>
 
 <script>
